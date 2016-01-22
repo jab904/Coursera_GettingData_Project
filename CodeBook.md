7dfb216798e56b54002ef5a5b3ee79e069c2df38
jab904 <judyya87@gmail.com>
2016-01-22 13:40:28 -0500

    Project

---
+++ CodeBook.md
@@ -0,0 +1,81 @@
+## Code book
+
+There are 180 rows and 67 columns in the final tidy data set.
+Each row corresponds to one subject doing one activity.
+
+### Variable names
+
+Column one is the subject and the activity that they were doing (ie 1.LAYING is subject one doing the activity of laying)
+The rest of the columns are mean or standard deviation for the measurements that were taken
+
+Please see https://www.coursera.org/learn/data-cleaning/discussions/eoUpRb-JEeW_UhLKov_F2Q
+There the TA said to use only letters and digits in column names so all .,-, and () were removed.
+
+1. Subjectactivity
+2. tBodyAccmeanX
+3. tBodyAccmeanY
+4. tBodyAccmeanZ
+5. tBodyAccstdX
+6. tBodyAccstdY
+7. tBodyAccstdZ
+8. tGravityAccmeanX
+9. tGravityAccmeanY
+10. tGravityAccmeanZ
+11. tGravityAccstdX
+12. tGravityAccstdY
+13. tGravityAccstdZ
+14. tBodyAccJerkmeanX
+15. tBodyAccJerkmeanY
+16. tBodyAccJerkmeanZ
+17. tBodyAccJerkstdX
+18. tBodyAccJerkstdY
+19. tBodyAccJerkstdZ
+20. tBodyGyromeanX
+21. tBodyGyromeanY
+22. tBodyGyromeanZ
+23. tBodyGyrostdX
+24. tBodyGyrostdY
+25. tBodyGyrostdZ
+26. tBodyGyroJerkmeanX
+27. tBodyGyroJerkmeanY
+28. tBodyGyroJerkmeanZ
+29. tBodyGyroJerkstdX
+30. tBodyGyroJerkstdY
+31. tBodyGyroJerkstdZ
+32. tBodyAccMagmean
+33. tBodyAccMagstd
+34. tGravityAccMagmean
+35. tGravityAccMagstd
+36. tBodyAccJerkMagmean
+37. tBodyAccJerkMagstd
+38. tBodyGyroMagmean
+39. tBodyGyroMagstd
+40. tBodyGyroJerkMagmean
+41. tBodyGyroJerkMagstd
+42. fBodyAccmeanX
+43. fBodyAccmeanY
+44. fBodyAccmeanZ
+45. fBodyAccstdX
+46. fBodyAccstdY
+47. fBodyAccstdZ
+48. fBodyAccJerkmeanX
+49. fBodyAccJerkmeanY
+50. fBodyAccJerkmeanZ
+51. fBodyAccJerkstdX
+52. fBodyAccJerkstdY
+53. fBodyAccJerkstdZ
+54. fBodyGyromeanX
+55. fBodyGyromeanY
+56. fBodyGyromeanZ
+57. fBodyGyrostdX
+58. fBodyGyrostdY
+59. fBodyGyrostdZ
+60. fBodyAccMagmean
+61. fBodyAccMagstd
+62. fBodyBodyAccJerkMagmean
+63. fBodyBodyAccJerkMagstd
+64. fBodyBodyGyroMagmean
+65. fBodyBodyGyroMagstd
+66. fBodyBodyGyroJerkMagmean
+67. fBodyBodyGyroJerkMagstd
+
---
+++ run_analysis.R
@@ -0,0 +1,62 @@
+## Set working directory
+setwd("C:/Users/JEB/Desktop/Coursera")
+if (!file.exists("data")) {
+  dir.create("data")
+}
+fileUrl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
+## Download the files from the web and unzip them
+download.file(fileUrl, dest=".\\data\\files.zip", mode="wb")
+unzip(zipfile=".\\data\\files.zip", exdir=".\\data")
+
+## Read the files
+filelist=list.files("data/UCI HAR Dataset", full.names = T)
+activity_labels<-read.table(filelist[1], header=F)
+features<-read.table(filelist[2], header=F)
+
+filelisttest=list.files("data/UCI HAR Dataset/test", full.names = T)
+test_subject<-read.table(filelisttest[2], header=F)
+test_data<-read.table(filelisttest[3], header=F)
+test_activity<-read.table(filelisttest[4], header=F)
+
+filelisttrain=list.files("data/UCI HAR Dataset/train", full.names = T)
+train_subject<-read.table(filelisttrain[2], header=F)
+train_data<-read.table(filelisttrain[3], header=F)
+train_activity<-read.table(filelisttrain[4], header=F)
+
+## Merge training and test data
+merged_data<-rbind(train_data, test_data)
+merged_subjects<-rbind(train_subject, test_subject)
+names(merged_subjects)<-"Subject"
+merged_activity<-rbind(train_activity, test_activity)
+names(merged_activity)<-"Activity"
+
+## Add column names
+y<-as.vector(features$V2)
+names(merged_data)<-y
+
+## Extract only mean and standard deviation, and add subject and activity column
+mean_std<-grep("\\mean()\\b|std()", names(merged_data))
+merged_data<-merged_data[,mean_std]
+merged_data<-cbind(merged_subjects, merged_activity, merged_data)
+
+## Clean up column names
+names(merged_data)<-gsub("\\(\\)","", names(merged_data))
+names(merged_data)<-gsub("-","", names(merged_data))
+
+## Add activity names
+x<-as.vector(activity_labels$V1)
+z<-as.vector(activity_labels$V2)
+
+for (i in 1:length(x)) {
+  merged_data$Activity<-gsub(x[i], z[i], merged_data$Activity)
+}
+
+## Add new column combinding subject and activey and remove the two old columns
+merged_data$Subjectactivity<-with(merged_data, paste(Subject, Activity, sep="."))
+merged_data$Subject<-NULL
+merged_data$Activity<-NULL
+
+## Create new data set with average for each variable based on Subjectactivity
+q<-aggregate(.~Subjectactivity, merged_data, mean)
+write.table(q, "tidyUCIdata.txt", row.names = F)
